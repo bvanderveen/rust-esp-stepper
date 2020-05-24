3e6b9a4e616051cd6d54a3efff14162de5c16581
Benjamin van der Veen <benjamin@survantjames.com>
2020-05-24 00:41:41 -0700

    some WIP rust abstraction over motor control

--- src/main.rs
+++ src/main.rs
@@ -14,6 +14,7 @@ fn panic(_info: &PanicInfo) -> ! {
 
 const STEP_GPIO: gpio_num_t = gpio_num_t_GPIO_NUM_17;
 const DIRECTION_GPIO: gpio_num_t = gpio_num_t_GPIO_NUM_18;
+
 const BLINK_GPIO: gpio_num_t = gpio_num_t_GPIO_NUM_2;
 const UART_NUM: uart_port_t = uart_port_t_UART_NUM_0;
 //const ECHO_TEST_TXD: i32 = gpio_num_t_GPIO_NUM_17 as i32;
@@ -32,15 +33,53 @@ pub fn app_main() {
     }
 }
 
+mod motor {
+    struct Config{step_gpio: gpio_num_t, direction_gpio: gpio_num_t, rmt_channel: rmt_channel_t)
+    struct LevelDuration(level: u32, duration: u32)
+
+    impl Config {
+        pub fn initialize(&self) {
+            gpio_pad_select_gpio(self.step_gpio as u8);
+            gpio_set_direction(self.step_gpio, gpio_mode_t_GPIO_MODE_OUTPUT);
+
+            gpio_pad_select_gpio(self.direction_gpio as u8);
+            gpio_set_direction(self.direction_gpio, gpio_mode_t_GPIO_MODE_OUTPUT);
+            
+            rmt_init(self.rmt_channel, self.step_gpio);
+        }
+
+        pub fn set_direction(&self, bool direction) {
+            gpio_set_level(self.direction_gpio, if direction { 1 } else { 0 } as u32);
+        }
+
+        pub fn write_items(&self, items: &[rmt_item32_t]) {
+            rmt_write_items(self.rmt_channel, items.as_ptr(), items.len(), false);  
+        }
+    }
+    
+}
+
+pub use self::motor::Config
 unsafe fn rust_blink_and_write() {
-    gpio_pad_select_gpio(STEP_GPIO as u8);
-    gpio_set_direction(STEP_GPIO, gpio_mode_t_GPIO_MODE_OUTPUT);
+    //gpio_pad_select_gpio(STEP_GPIO as u8);
+    //gpio_set_direction(STEP_GPIO, gpio_mode_t_GPIO_MODE_OUTPUT);
 
-    gpio_pad_select_gpio(DIRECTION_GPIO as u8);
-    gpio_set_direction(DIRECTION_GPIO, gpio_mode_t_GPIO_MODE_OUTPUT);
+    //gpio_pad_select_gpio(DIRECTION_GPIO as u8);
+    //gpio_set_direction(DIRECTION_GPIO, gpio_mode_t_GPIO_MODE_OUTPUT);
 
-    /* Configure parameters of an UART driver,
-     * communication pins and install the driver */
+    let motor_x = motor::Config { 
+        step_gpio: gpio_num_t_GPIO_NUM_17; 
+        direction_gpio: gpio_num_t_GPIO_NUM_18;
+    };
+
+    let motor_y = motor::Config { 
+        step_gpio: gpio_num_t_GPIO_NUM_17; 
+        direction_gpio: gpio_num_t_GPIO_NUM_18;
+    };
+
+    motor_x.initialize();
+    motor_y.initialize();
+
     let uart_config = uart_config_t {
         baud_rate: 115200,
         data_bits: uart_word_length_t_UART_DATA_8_BITS,
@@ -59,23 +98,27 @@ unsafe fn rust_blink_and_write() {
 
     let channel = rmt_channel_t_RMT_CHANNEL_0;
 
-    rmt_init(channel, STEP_GPIO);
+    //rmt_init(channel, STEP_GPIO);
     let steps_per_second = 40 * 2;
     let milliseconds_per_second = 1000;
     let milliseconds_per_step = milliseconds_per_second / steps_per_second;
     let step_period = milliseconds_per_step / tick_period_ms;
 
     let mut i = 0 as u32;
-    let mut direction = 0 as u32;
+    let mut direction = false;
 
-    gpio_set_level(DIRECTION_GPIO, direction);
+    //gpio_set_level(DIRECTION_GPIO, direction);
+    motor_x.set_direction(direction);
+    motor_y.set_direction(direction);
     loop {
         i = i + 1;
         if i > 10 {
             i = 0;
-            direction = if direction == 0 { 1 } else { 0 };
-            gpio_set_level(DIRECTION_GPIO, direction);
+            direction = !direction; 
+            motor_x.set_direction(direction);
+            motor_y.set_direction(direction);
         }
+
 
         //gpio_set_level(STEP_GPIO, 0);
 
@@ -99,11 +142,15 @@ unsafe fn rust_blink_and_write() {
                 item(32767,0,32767,0)
         ];
 
-       rmt_write_items(channel, messages.as_ptr(), 8, false);
+
+       motor_x.write_items(&messages);
+       motor_y.write_items(&messages);
+
+//rmt_write_items(channel, messages.as_ptr(), 8, false);
     }
 }
 
-unsafe fn item(a: u32, b: u32, c: u32, d: u32) -> rmt_item32_t{
+unsafe fn item(a: u32, b: u32, c: u32, d: u32) -> rmt_item32_t {
         let message = rmt_item32_t {
             __bindgen_anon_1: rmt_item32_t__bindgen_ty_1 {
                 __bindgen_anon_1: rmt_item32_t__bindgen_ty_1__bindgen_ty_1 { _bitfield_1: rmt_item32_t__bindgen_ty_1__bindgen_ty_1::new_bitfield_1(a, b, c ,d) },
